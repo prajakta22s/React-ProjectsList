24c3f22ebed6d313744fd466f078e4f2170bfd76
prajakta22s <prajakta22sb@gmail.com>
2024-06-25 15:31:58 +0530

    first commit

---
+++ rtktoDo/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddTodo from './componets/AddTodo'
+import Todos from './componets/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Learn React-ReduxToolkit</h1>
+      <AddTodo/>
+      <Todos/>
+      
+    </>
+  )
+}
+
+export default App
